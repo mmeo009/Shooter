2ebf46de3a715a9a81266137a5c4bc7bf8387a60
mmeo009 <shh020615@gmail.com>
2023-08-14 11:32:12 +0900

    진짜 할일이 없어서...

--- SV_Shoothing/Assets/02.Scripts/BasicMove.cs
+++ SV_Shoothing/Assets/02.Scripts/BasicMove.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class BasicMove : MonoBehaviour
 {
     public float moveSpeed;
+    public float rotSpeed;
 
     private Rigidbody2D rb;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rotSpeed = Random.Range(10f, 100f);
     }
 
     void Update()
     {
-        rb.velocity = new Vector2(moveSpeed, 0);   
+        rb.velocity = new Vector2(moveSpeed, 0);
+        transform.Rotate(0, 0, this.rotSpeed * Time.deltaTime);
     }
 }
